ea6888fe417d0d222284cb13130abba0da27fd43
pengyunfei <stack_admin@163.com>
2019-12-21 23:04:58 +0800

    cache-autopath

--- src/json.js
+++ src/json.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const fs = require('fs');
-const vm  = require('vm');
+const vm = require('vm');
 
 function Module(id) {
     this.id = id;
@@ -16,8 +16,7 @@ Module._extenstions = {
         const content = fs.readFileSync(module.id, 'utf8');
         const contentStr = Module.wrap[0] + content + Module.wrap[1]
         const fn = vm.runInThisContext(contentStr)
-        fn.call()
-        console.log(fn())
+        fn.call(module.exports, module.exports, module, req)
     },
     '.json'(module) {
         // console.log(module)
@@ -26,19 +25,46 @@ Module._extenstions = {
     }
 }
 
-
 function tryModuleLoad(module) {
     // console.log(module)
     const extension = path.extname(module.id, 'utf8')
     Module._extenstions[extension](module)
 }
-
+// 缓存 避免二次读取
+Module._cache = {}
 function req(modulePath) {
     let absPathName = path.resolve(__dirname, modulePath);
+    let extNames = Object.keys(Module._extenstions);
+    let index = -1;
+    let oldAbsPathName = absPathName
+    function find(absPathName) {
+        if (index === extNames.length) {
+            return absPathName
+        }
+        try {
+            fs.accessSync(absPathName)
+            return absPathName
+        } catch (e) {
+            let ext = extNames[index++] //.js
+            let netPath = oldAbsPathName + ext //path + .js
+            return find(netPath)
+        }
+    }
+    absPathName = find(absPathName)
+    try {
+        fs.accessSync(absPathName)
+    } catch (e) {
+        throw new Error('🚀')
+    }
+    if (Module._cache[absPathName]) {
+        return Module._cache[absPathName].exports
+    }
     let module = new Module(absPathName);
+    Module._cache[absPathName] = module
     tryModuleLoad(module);
     return module.exports;
 }
 // let obj = req('./json.json')
-let js = req('./js.js')
-// console.log(obj);
+let obj = req('./js')
+// obj = req('./js.js')
+console.log(obj);
